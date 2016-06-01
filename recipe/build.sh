19cd4c68103bd51bb94f77b8baf6026e0e80dbe0
Gregory R. Lee <gregory.lee@cchmc.org>
2016-06-01 14:22:33 -0400

    build METIS first, install it then use the installed METIS when building SuiteSparse

--- recipe/build.sh
+++ recipe/build.sh
@@ -21,20 +21,35 @@ export INSTALL_INCLUDE="${PREFIX}/include"
 export BLAS="-lopenblas"
 export LAPACK="-lopenblas"
 
+# build METIS first, installing into $PREFIX rather than the source DIR
+cd metis-5.1.0
+make config shared=1 prefix=${PREFIX}
+make
+make install
+
+if [ "$(uname)" == "Darwin" ]
+then
+    # this is copied from the SuiteSparse Makefile
+    install_name_tool -id ${PREFIX}/lib/libmetis.dylib ${PREFIX}/lib/libmetis.dylib
+fi
+
+# also build static version of METIS
+make config prefix=${PREFIX}
+make
+make install
+cd ..
+
+# export environment variable so SuiteSparse will use the METIS built above
+export MY_METIS_LIB="-L${PREFIX}/lib -lmetis"
+
 # (optional) write out various make variables for easier build debugging
 eval ${LIBRARY_SEARCH_VAR}="${PREFIX}/lib" make config 2>&1 | tee make_config.txt
 
+# make SuiteSparse
 eval ${LIBRARY_SEARCH_VAR}="${PREFIX}/lib" make -j1
-
-# make install below fails to link libmetis unless it is copied to $PREFIX/lib
-cp $SRC_DIR/lib/libmetis$DYNAMIC_EXT $PREFIX/lib
-
 make install
 
-
 if [ "$(uname)" == "Darwin" ]; then
-
   # change link to METIS for CHOLMOD to reflect install path
   install_name_tool -change $SRC_DIR/lib/libmetis.dylib @rpath/libmetis.dylib $PREFIX/lib/libcholmod.dylib
-
 fi
