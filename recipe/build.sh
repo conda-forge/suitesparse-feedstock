23dd5e2bd52f7d8099d759d9bed614c491ac3008
ngam <67342040+ngam@users.noreply.github.com>
2024-02-03 11:50:22 -0500

    build anew

--- recipe/build.sh
+++ recipe/build.sh
@@ -17,44 +17,54 @@ fi
 export LDFLAGS=${LDFLAGS/-Wl,--as-needed/}
 export LDFLAGS=${LDFLAGS/-Wl,-dead_strip_dylibs/}
 
-export INSTALL="${PREFIX}"
-# continue to ignore docs
-export INSTALL_DOC="${SRC_DIR}/doc"
-# make sure CMake install goes in the right place
-export CMAKE_OPTIONS="-DCMAKE_INSTALL_PREFIX=${PREFIX} -DCMAKE_INSTALL_LIBDIR=lib"
+mkdir -p build && cd build
+cmake \
+    -DCMAKE_INSTALL_PREFIX=${PREFIX} \
+    -DCMAKE_BUILD_TYPE=Release \
+    -DSUITESPARSE_USE_CUDA=OFF \
+    -DSUITESPARSE_INCLUDEDIR_POSTFIX=${PREFIX}/include \
+    ..
+cmake --build .
+cmake --install .
 
-export BLAS="-lblas -llapack"
-export LAPACK="-lblas -llapack"
+# export INSTALL="${PREFIX}"
+# # continue to ignore docs
+# export INSTALL_DOC="${SRC_DIR}/doc"
+# # make sure CMake install goes in the right place
+# export CMAKE_OPTIONS="-DCMAKE_INSTALL_PREFIX=${PREFIX} -DCMAKE_INSTALL_LIBDIR=lib"
 
-export CUDA="no"
+# export BLAS="-lblas -llapack"
+# export LAPACK="-lblas -llapack"
 
-# export environment variable so SuiteSparse will use the METIS built above
-export MY_METIS_LIB="-L${PREFIX}/lib -lmetis -Wl,-rpath,$PREFIX/lib"
-export MY_METIS_INC="-I${PREFIX}/include"
+# export CUDA="no"
 
-# (optional) write out various make variables for easier build debugging
-make config 2>&1 | tee make_config.txt
+# # export environment variable so SuiteSparse will use the METIS built above
+# export MY_METIS_LIB="-L${PREFIX}/lib -lmetis -Wl,-rpath,$PREFIX/lib"
+# export MY_METIS_INC="-I${PREFIX}/include"
 
-# skip graphblas, mongoose by giving them a no-op makefile
-cp -v ${RECIPE_DIR}/Makefile.empty GraphBLAS/Makefile
-cp -v ${RECIPE_DIR}/Makefile.empty Mongoose/Makefile
+# # (optional) write out various make variables for easier build debugging
+# make config 2>&1 | tee make_config.txt
 
-# make SuiteSparse
-make library static VERBOSE=1
-make install
+# # skip graphblas, mongoose by giving them a no-op makefile
+# cp -v ${RECIPE_DIR}/Makefile.empty GraphBLAS/Makefile
+# cp -v ${RECIPE_DIR}/Makefile.empty Mongoose/Makefile
 
-# manually install the static libraries
-cp ${SRC_DIR}/AMD/Lib/libamd.a ${PREFIX}/lib
-cp ${SRC_DIR}/BTF/Lib/libbtf.a ${PREFIX}/lib
-cp ${SRC_DIR}/CAMD/Lib/libcamd.a ${PREFIX}/lib
-cp ${SRC_DIR}/CCOLAMD/Lib/libccolamd.a ${PREFIX}/lib
-cp ${SRC_DIR}/CHOLMOD/Lib/libcholmod.a ${PREFIX}/lib
-cp ${SRC_DIR}/COLAMD/Lib/libcolamd.a ${PREFIX}/lib
-cp ${SRC_DIR}/CSparse/Lib/libcsparse.a ${PREFIX}/lib
-cp ${SRC_DIR}/CXSparse/Lib/libcxsparse.a ${PREFIX}/lib
-cp ${SRC_DIR}/KLU/Lib/libklu.a ${PREFIX}/lib
-cp ${SRC_DIR}/LDL/Lib/libldl.a ${PREFIX}/lib
-cp ${SRC_DIR}/RBio/Lib/librbio.a ${PREFIX}/lib
-cp ${SRC_DIR}/SPQR/Lib/libspqr.a ${PREFIX}/lib
-cp ${SRC_DIR}/SuiteSparse_config/libsuitesparseconfig.a ${PREFIX}/lib
-cp ${SRC_DIR}/UMFPACK/Lib/libumfpack.a ${PREFIX}/lib
+# # make SuiteSparse
+# make library static VERBOSE=1
+# make install
+
+# # manually install the static libraries
+# cp ${SRC_DIR}/AMD/Lib/libamd.a ${PREFIX}/lib
+# cp ${SRC_DIR}/BTF/Lib/libbtf.a ${PREFIX}/lib
+# cp ${SRC_DIR}/CAMD/Lib/libcamd.a ${PREFIX}/lib
+# cp ${SRC_DIR}/CCOLAMD/Lib/libccolamd.a ${PREFIX}/lib
+# cp ${SRC_DIR}/CHOLMOD/Lib/libcholmod.a ${PREFIX}/lib
+# cp ${SRC_DIR}/COLAMD/Lib/libcolamd.a ${PREFIX}/lib
+# cp ${SRC_DIR}/CSparse/Lib/libcsparse.a ${PREFIX}/lib
+# cp ${SRC_DIR}/CXSparse/Lib/libcxsparse.a ${PREFIX}/lib
+# cp ${SRC_DIR}/KLU/Lib/libklu.a ${PREFIX}/lib
+# cp ${SRC_DIR}/LDL/Lib/libldl.a ${PREFIX}/lib
+# cp ${SRC_DIR}/RBio/Lib/librbio.a ${PREFIX}/lib
+# cp ${SRC_DIR}/SPQR/Lib/libspqr.a ${PREFIX}/lib
+# cp ${SRC_DIR}/SuiteSparse_config/libsuitesparseconfig.a ${PREFIX}/lib
+# cp ${SRC_DIR}/UMFPACK/Lib/libumfpack.a ${PREFIX}/lib
