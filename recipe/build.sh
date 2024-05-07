a6146e48ee22bd2bc7751896c3c6e78ed3252ef8
Julien Schueller <schueller@phimeca.com>
2024-05-07 11:35:22 +0200

    Try all libs

--- recipe/build.sh
+++ recipe/build.sh
@@ -9,16 +9,15 @@
 export LDFLAGS=${LDFLAGS/-Wl,--as-needed/}
 export LDFLAGS=${LDFLAGS/-Wl,-dead_strip_dylibs/}
 
-export CMAKE_ARGS="${CMAKE_ARGS} -DSUITESPARSE_USE_CUDA=OFF"
-
 if [[ "${target_platform}" != "${build_platform}" ]]; then
   export CMAKE_ARGS="${CMAKE_ARGS} -DCMAKE_CROSSCOMPILING=ON"
 fi
 
 # add
 # can add lagraph and -DSUITESPARSE_USE_SYSTEM_GRAPHBLAS after packaging 9.1
-cmake -B build \
-  -DSUITESPARSE_ENABLE_PROJECTS="suitesparse_config;amd;btf;camd;ccolamd;colamd;cholmod;cxsparse;ldl;klu;umfpack;paru;rbio;spqr;spex" \
+cmake -B build -LAH \
+  -DSUITESPARSE_USE_CUDA=OFF \
+  -DGRAPHBLAS_COMPACT=ON \
   -DBLA_VENDOR="Generic" \
   -DBLAS_LIBRARIES="$PREFIX/lib/libblas${SHLIB_EXT};$PREFIX/lib/libcblas${SHLIB_EXT}" \
   -DBUILD_SHARED_LIBS=ON \
