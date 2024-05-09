98c61b3bd014401954f5f2ad435343cb52e2e13c
Min RK <benjaminrk@gmail.com>
2024-05-09 20:31:53 +0200

    find blas

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -5,6 +5,8 @@ cmake -B build ^
   -DSUITESPARSE_ENABLE_PROJECTS="suitesparse_config;amd;btf;camd;ccolamd;colamd;cholmod;cxsparse;ldl;klu;umfpack;paru;rbio;spqr;spex" ^
   -DBUILD_SHARED_LIBS=ON ^
   -DBUILD_STATIC_LIBS=OFF ^
+  -DBLA_VENDOR=Generic ^
+  -DBLAS_LIBRARIES="%LIBRARY_PREFIX%\bin\blas.dll;%LIBRARY_PREFIX%\bin\cblas.dll" \
   -DCMAKE_BUILD_TYPE:STRING=Release ^
   -DCMAKE_INSTALL_PREFIX:PATH="%LIBRARY_PREFIX%" ^
   -DCMAKE_PREFIX_PATH:PATH="%LIBRARY_PREFIX%" ^
