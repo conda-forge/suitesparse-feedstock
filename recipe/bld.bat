d4c43e61893230b50b599273e81fa6572b8a5e0f
Min RK <benjaminrk@gmail.com>
2024-05-10 11:10:14 +0200

    Find blas with blas-devel
    Co-authored-by: Silvio Traversaro <silvio@traversaro.it>

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -3,10 +3,8 @@ setlocal EnableDelayedExpansion
 cmake -B build ^
   -G "Ninja" ^
   -DSUITESPARSE_ENABLE_PROJECTS="suitesparse_config;amd;btf;camd;ccolamd;colamd;cholmod;cxsparse;ldl;klu;umfpack;paru;rbio;spqr" ^
-  -DBLA_VENDOR=OpenBLAS ^
   -DBUILD_SHARED_LIBS=ON ^
   -DBUILD_STATIC_LIBS=OFF ^
-  -DCMAKE_WINDOWS_EXPORT_ALL_SYMBOLS=ON ^
   -DCMAKE_BUILD_TYPE:STRING=Release ^
   -DCMAKE_INSTALL_PREFIX:PATH="%LIBRARY_PREFIX%" ^
   -DCMAKE_PREFIX_PATH:PATH="%LIBRARY_PREFIX%" ^
