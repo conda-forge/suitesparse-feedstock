0498432e5b0d19de340332005802fb6ff666b9ab
Min RK <benjaminrk@gmail.com>
2024-11-12 21:04:26 +0100

    maybe blas/lapack_libraries aren't needed

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -4,8 +4,6 @@ cmake -B build ^
   -G "Ninja" ^
   -DSUITESPARSE_ENABLE_PROJECTS="suitesparse_config;amd;btf;camd;ccolamd;colamd;cholmod;cxsparse;ldl;klu;umfpack;paru;rbio;spqr" ^
   -DBLA_VENDOR=Generic ^
-  -DBLAS_LIBRARIES="blas;cblas" ^
-  -DLAPACK_LIBRARIES="lapack" ^
   -DBUILD_SHARED_LIBS=ON ^
   -DBUILD_STATIC_LIBS=OFF ^
   -DCMAKE_BUILD_TYPE:STRING=Release ^
