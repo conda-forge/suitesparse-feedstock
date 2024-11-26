8adf26f99511bf8953ff0a0924cafb7a598700bf
Min RK <benjaminrk@gmail.com>
2024-11-26 12:06:54 +0100

    single build, multiple outputs (adds suitesparse-devel for headers, etc.)

--- recipe/build-all.bat
+++ recipe/build-all.bat
@@ -1,27 +1,17 @@
 setlocal EnableDelayedExpansion
 
-where flang.exe
-if not errorlevel 1 (
-  echo using fortran
-  set "CMAKE_ARGS=!CMAKE_ARGS! -DSUITESPARSE_HAS_FORTRAN:BOOL=ON -DCMAKE_Fortran_COMPILER=flang.exe"
-) else (
-  echo no fortran
-  set "CMAKE_ARGS=!CMAKE_ARGS! -DSUITESPARSE_HAS_FORTRAN:BOOL=OFF"
-)
-
-cd "%SUBPKG_DIR%"
-if errorlevel 1 exit 1
-
 cmake -B build ^
   -G "Ninja" ^
+  -DSUITESPARSE_ENABLE_PROJECTS="suitesparse_config;amd;btf;camd;ccolamd;colamd;cholmod;cxsparse;ldl;klu;umfpack;paru;rbio;spqr;spex;mongoose" ^
   -DBLA_VENDOR=Generic ^
   -DBUILD_SHARED_LIBS=ON ^
   -DBUILD_STATIC_LIBS=OFF ^
   -DCMAKE_BUILD_TYPE:STRING=Release ^
   -DCMAKE_INSTALL_PREFIX:PATH="%LIBRARY_PREFIX%" ^
   -DCMAKE_PREFIX_PATH:PATH="%LIBRARY_PREFIX%" ^
-  !CMAKE_ARGS!
-
+  -DSUITESPARSE_HAS_FORTRAN:BOOL=ON ^
+  -DCMAKE_Fortran_COMPILER=flang.exe ^
+  %CMAKE_ARGS%
 if errorlevel 1 exit 1
 
 cmake --build build --verbose
@@ -29,4 +19,3 @@ if errorlevel 1 exit 1
 
 cmake --install build --verbose
 if errorlevel 1 exit 1
-
--- recipe/build-all.sh
+++ recipe/build-all.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 set -eux
-# common build script for all subpackages
 
 # conda compilers strip links that aren't used by default,
 # even if explicitly given.
@@ -16,26 +15,12 @@ if [[ "${target_platform}" != "${build_platform}" ]]; then
   export CMAKE_ARGS="${CMAKE_ARGS} -DCMAKE_CROSSCOMPILING=ON"
 fi
 
-if [[ -z "${FC:-}" || ! -f "${FC:-fc_compiler}" ]]; then
-  echo "Not using fortran"
-  export CMAKE_ARGS="${CMAKE_ARGS} -DSUITESPARSE_USE_FORTRAN=OFF"
-fi
-
-cd "${SUBPKG_DIR}"
-
+# graphblas, lagraph packaged separately
 cmake -B build \
+  -DSUITESPARSE_ENABLE_PROJECTS="suitesparse_config;amd;btf;camd;ccolamd;colamd;cholmod;cxsparse;ldl;klu;umfpack;paru;rbio;spqr;spex;mongoose" \
   -DBLA_VENDOR="Generic" \
   -DBUILD_SHARED_LIBS=ON \
   -DBUILD_STATIC_LIBS=OFF \
   ${CMAKE_ARGS}
 cmake --build build --parallel "${CPU_COUNT:-1}"
 cmake --install build --verbose
-
-# verify subpackage versions
-if [[ "${PKG_NAME}" == lib* ]]; then
-  if [[ "$target_platform" == osx-* ]]; then
-    test -f ${PREFIX}/lib/${PKG_NAME}.${PKG_VERSION}${SHLIB_EXT}
-  else
-    test -f ${PREFIX}/lib/${PKG_NAME}${SHLIB_EXT}.${PKG_VERSION}
-  fi
-fi
