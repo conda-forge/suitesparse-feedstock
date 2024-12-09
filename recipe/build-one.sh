2b94e10ee922dcdb42016cebf8b233a97324002b
Min RK <benjaminrk@gmail.com>
2024-12-09 09:34:22 +0100

    verify project version directly
    instead of SOVERSION

--- recipe/build-one.sh
+++ recipe/build-one.sh
@@ -29,14 +29,17 @@ cmake -B build \
   -DBUILD_SHARED_LIBS=ON \
   -DBUILD_STATIC_LIBS=OFF \
   ${CMAKE_ARGS}
+
+# verify subpackage version, since it doesn't get updated automatically
+# line in CMakeCache looks like:
+# //Value Computed by CMake
+# CMAKE_PROJECT_VERSION:STATIC=3.3.3
+
+PROJECT_VERSION=$(grep "CMAKE_PROJECT_VERSION:" build/CMakeCache.txt | cut -d= -f2)
+if [[ "${PROJECT_VERSION}" != "${PKG_VERSION}" ]]; then
+  echo "CMake Project version ${PROJECT_VERSION} != conda package version ${PKG_VERSION}, update package version for ${PKG_NAME}=${PROJECT_VERSION}"
+  exit 1
+fi
+
 cmake --build build --parallel "${CPU_COUNT:-1}"
 cmake --install build --verbose
-
-# verify subpackage versions
-if [[ "${PKG_NAME}" == lib* ]]; then
-  if [[ "$target_platform" == osx-* ]]; then
-    test -f ${PREFIX}/lib/${PKG_NAME}.${PKG_VERSION}${SHLIB_EXT}
-  else
-    test -f ${PREFIX}/lib/${PKG_NAME}${SHLIB_EXT}.${PKG_VERSION}
-  fi
-fi
